bdf4505cc4fad4e3723bc98e43a2150d37ba4094
Shrivu Shankar <shrivu1122@gmail.com>
2018-08-10 12:36:13 -0500

    Added Orders

--- Tradinhood/Robinhood.py
+++ Tradinhood/Robinhood.py
@@ -11,7 +11,7 @@ ENDPOINTS = {
     'orders': 'https://api.robinhood.com/orders/',
     'holdings': 'https://nummus.robinhood.com/holdings/',
     'instruments': 'https://api.robinhood.com/instruments/',
-    'nummus_order': 'https://nummus.robinhood.com/orders/',
+    'nummus_orders': 'https://nummus.robinhood.com/orders/',
     'currency_pairs': 'https://nummus.robinhood.com/currency_pairs/',
     'nummus_accounts': 'https://nummus.robinhood.com/accounts/',
     'forex_market_quote': 'https://api.robinhood.com/marketdata/forex/quotes/'
@@ -78,8 +78,8 @@ class Robinhood:
         """Inits internal account information from Robinhood
 
         Args:
-            acc_num: (str, optional) Manual specify the account number
-            nummus_id: (str, optional) Manual specify the nummus id
+            acc_num: (str, optional) manually specify the account number
+            nummus_id: (str, optional) manually specify the nummus id
         """
         assert self.logged_in
 
@@ -101,12 +101,12 @@ class Robinhood:
         """Login/Authenticate
 
         Args:
-            token: (str) Required if username/password not given, bypasses login
+            token: (str) required if username/password not given, bypasses login
                 since API token already known
-            username: (str) Required login information if token not specified
-            password: (str) Required login information if token not specified
-            acc_num: (str, optional) Manual specify the account number
-            nummus_id: (str, optional) Manual specify the nummus id
+            username: (str) required login information if token not specified
+            password: (str) required login information if token not specified
+            acc_num: (str, optional) manual specify the account number
+            nummus_id: (str, optional) manual specify the nummus id
 
         Returns:
             (bool) If login was successful
@@ -191,8 +191,8 @@ class Robinhood:
         """Get owned quantity of asset
 
         Args:
-            asset: (Currency | Stock | str) The query currency/stock or symbol
-            include_held: (bool, optional) Whether to included held assets in the tally
+            asset: (Currency | Stock | str) the query currency/stock or symbol
+            include_held: (bool, optional) whether to included held assets in the tally
 
         Returns:
             (Decimal) Quantity of asset owned
@@ -247,7 +247,7 @@ class Robinhood:
         else:
             raise UsageError('Invalid asset')
 
-    def _order(self, order_side, asset, amt, type='market', price=None, stop_price=None, time_in_force='gtc'):
+    def _order(self, order_side, asset, amt, type='market', price=None, stop_price=None, time_in_force='gtc', return_json=False):
         """Internal order method
 
         See .buy(...) and .sell(...)
@@ -284,8 +284,16 @@ class Robinhood:
                 'time_in_force': time_in_force
             }
 
-            res = self.session.post(ENDPOINTS['nummus_order'], json=req_json)
-            return res.json()
+            res = self.session.post(ENDPOINTS['nummus_orders'], json=req_json)
+            res_json = res.json()
+
+            if 'error_code' in res_json:
+                raise APIError(res_json['error_code'])
+
+            if return_json:
+                return res_json
+            else:
+                return Order(self.session, res_json, 'cryptocurrency', symbol=asset.symbol)
 
         elif isinstance(asset, Stock):
 
@@ -321,7 +329,15 @@ class Robinhood:
                 req_json['stop_price'] = stop_price
 
             res = self.session.post(ENDPOINTS['orders'], json=req_json)
-            return res.json()
+            res_json = res.json()
+
+            if 'error_code' in res_json:
+                raise APIError(res_json['error_code'])
+
+            if return_json:
+                return res_json
+            else:
+                return Order(self.session, res_json, 'stock', symbol=asset.symbol)
 
         else:
             raise UsageError('Invalid asset')
@@ -330,17 +346,18 @@ class Robinhood:
         """Buy item
 
         Args:
-            asset: (Currency | Stock | str) The asset to be bought
-            amt: (Decimal | float | int) The amt to buy
-            type: (str, optional) The order type
+            asset: (Currency | Stock | str) the asset to be bought
+            amt: (Decimal | float | int) the amt to buy
+            type: (str, optional) the order type
                 ['market', 'limit', 'stoploss', 'stoplimit']
-            price: (Decimal | float | int) The order price
-            stop_price: (Decimal | float | int) The stop price, required if using stoploss/stoplimit
-            time_in_force: (str, optional) When to cancel
+            price: (Decimal | float | int) the order price
+            stop_price: (Decimal | float | int) the stop price, required if using stoploss/stoplimit
+            time_in_force: (str, optional) when to cancel
                 ['gtc', 'gfd', 'ioc', 'opg']
+            return_json: (bool) override return with API response
 
         Returns:
-            (dict) The API response
+            (Order) The order created
 
         Raises:
             UsageError: If used incorrectly...
@@ -351,22 +368,48 @@ class Robinhood:
         """Sell item
 
         Args:
-            asset: (Currency | Stock | str) The asset to be sold
+            asset: (Currency | Stock | str) tthe asset to be sold
             amt: (Decimal | float | int) The amt to sell
-            type: (str, optional) The order type
+            type: (str, optional) the order type
                 ['market', 'limit', 'stoploss', 'stoplimit']
-            price: (Decimal | float | int) The order price
-            stop_price: (Decimal | float | int) The stop price, required if using stoploss/stoplimit
-            time_in_force: (str, optional) When to cancel
+            price: (Decimal | float | int) the order price
+            stop_price: (Decimal | float | int) the stop price, required if using stoploss/stoplimit
+            time_in_force: (str, optional) when to cancel
                 ['gtc', 'gfd', 'ioc', 'opg']
+            return_json: (bool) override return with API response
 
         Returns:
-            (dict) The API response
+            (Order) The order created
 
         Raises:
             UsageError: If used incorrectly...
         """
         return self._order('sell', asset, amt, **kwargs)
+
+    @property
+    def orders(self, return_json=False):
+        """Get order history"""
+        assert self.logged_in
+
+        try:
+
+            res_stocks = self.session.get(ENDPOINTS['orders'])
+            res_crypto = self.session.get(ENDPOINTS['nummus_orders'])
+            res_stocks.raise_for_status()
+            res_crypto.raise_for_status()
+            json_stocks = res_stocks.json()
+            json_crypto = res_crypto.json()
+
+            if return_json:
+                return [json_stocks, json_crypto]
+
+            orders = [ Order(self.session, json_data, 'stock') for json_data in json_stocks['results'] ]
+            orders += [ Order(self.session, json_data, 'cryptocurrency') for json_data in json_crypto['results'] ]
+
+            return orders
+
+        except:
+            raise APIError('Unable to access orders')
 
     @property
     def account_info(self):
@@ -424,14 +467,14 @@ class Currency:
     """Currency asset object
 
     Attributes:
-        session: (Session) Current session used by the API
-        json: (dict) Internal data json
-        name: (str) Currency name
-        code: (str) Currency symbol
-        tradable: (bool) If tradable
+        session: (Session) current session used by the API
+        json: (dict) internal data json
+        name: (str) currency name
+        code: (str) currency symbol
+        tradable: (bool) if tradable
         type: (str) asset type
-        pair_id: (str) Currency Pair id
-        asset_id: (str) The APIs id for this currency
+        pair_id: (str) currency Pair id
+        asset_id: (str) the APIs id for this currency
     """
 
     def __init__(self, session, asset_json):
@@ -473,22 +516,22 @@ class Currency:
         return Decimal(self.current_quote['bid_price'])
 
     def __repr__(self):
-        return f'Currency<{self.name} [{self.code}]>'
+        return f'<Currency ({self.name}) [{self.code}]>'
 
 class Stock:
     """Stock asset object
 
     Attributes:
-        session: (Session) Current session used by the API
-        json: (dict) Internal data json
+        session: (Session) current session used by the API
+        json: (dict) internal data json
         name: (str) stock name
         simple_name: (str) simple stock name
-        code: (str) Currency symbol
-        symbol: (str) Currency symbol
-        tradable: (bool) If tradable
+        code: (str) currency symbol
+        symbol: (str) currency symbol
+        tradable: (bool) if tradable
         type: (str) asset type
-        instrument_url: (str) The instrument url for this stock
-        id: (str) The APIs id for this stock
+        instrument_url: (str) the instrument url for this stock
+        id: (str) the APIs id for this stock
     """
 
     def __init__(self, session, instrument_json):
@@ -530,4 +573,84 @@ class Stock:
         return Decimal(self.current_quote['bid_price'])
 
     def __repr__(self):
-        return f'Stock<{self.simple_name} [{self.symbol}]>'
+        return f'<Stock ({self.simple_name}) [{self.symbol}]>'
+
+class Order:
+    """Order object
+
+    Attributes:
+        session: (Session) current session used by the API
+        json: (dict) internal data json
+        id: (str) the order id
+        side: (str) buy or sell
+        time_in_force: (str) how the order in enforced
+        quantity: (Decimal) quantity of the asset
+        asset_type: (str) cryptocurrency or stock
+        cancel_url: (str) the url to cancel the order
+        price: (Decimal) the price set in the order,
+            this can be None
+        stop_price: (Deciaml) the stop price, None if not a stop order
+        symbol: (str) the symbol traded in the order, defaults None
+    """
+
+    def __init__(self, session, order_json, asset_type, symbol=None):
+
+        self.session = session
+        self.json = order_json
+
+        self.id = self.json['id']
+        self.side = self.json['side']
+        self.time_in_force = self.json['time_in_force']
+
+        self.quantity = Decimal(self.json['quantity'])
+        self.order_type = self.json['type']
+        self.asset_type = asset_type
+        self.symbol = symbol
+
+        if self.asset_type == 'cryptocurrency':
+            self.pair_id = self.json['currency_pair_id']
+            self.url = ENDPOINTS['nummus_orders'] + self.id
+        elif self.asset_type == 'stock':
+            self.instrument_url = self.json['instrument']
+            self.url = ENDPOINTS['orders'] + self.id
+
+        if 'cancel' in self.json:
+            self.cancel_url = self.json['cancel']
+        else:
+            self.cancel_url = self.json['cancel_url']
+
+        if self.json['price']:
+            self.price = Decimal(self.json['price'])
+        else:
+            self.price = None # price not set
+
+        if 'stop_price' in self.json and self.json['stop_price']:
+            self.stop_price = Decimal(self.json['stop_price'])
+        else:
+            self.stop_price = None # stop price not set
+
+    @property
+    def state(self):
+        """Get order state [confirmed, cancelled, filled]"""
+        try:
+            res = self.session.get(self.url)
+            res.raise_for_status()
+            res_json = res.json()
+            return res_json['state']
+        except:
+            raise APIError('Unable to access order data')
+
+    def cancel(self):
+        """Cancel this order"""
+        try:
+            res = self.session.post(self.cancel_url)
+            res.raise_for_status()
+            return res.json()
+        except:
+            raise APIError('Unable to cancel')
+
+    def __repr__(self):
+        if self.symbol:
+            return f'<Order ({self.id[:8]}) [{self.symbol}]>'
+        else:
+            return f'<Order ({self.id[:8]}) [...]>' # symbol has yet to be identified
